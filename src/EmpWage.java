e38d01c44e762c305692e7faffa8ae7a406e0d8e
SamarthBM <samarthbm18@gmail.com>
2021-06-30 14:27:20 +0530

    Added part time employee attendance and wage

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -5,15 +5,21 @@ public class EmpWage {
 		System.out.println("Welcome to employee wage computation program");
 		
 		int is_present_full = 1;
+		int is_present_part = 2;
 		int wage_per_hour = 20;
 		int working_hour = 0 ;
 		
-		double EmpCheck = Math.floor(Math.random() * 10) %2;
+		double EmpCheck = Math.floor(Math.random() * 10) %3;
 		
 		if (EmpCheck == is_present_full)
 		{
-			System.out.println("Employee is present");
+			System.out.println("Employee is present for full time");
 			working_hour = 8;
+		}
+		else if (EmpCheck == is_present_part)
+		{
+			System.out.println("Employee is present for part time");
+			working_hour = 4;
 		}
 		else
 		{
