59d9783ad764b002e9626de51bd31aa4485c82ba
SamarthBM <samarthbm18@gmail.com>
2021-06-30 14:30:10 +0530

    Used switch case instead of if and else if

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -4,25 +4,27 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage computation program");
 		
-		int is_present_full = 1;
-		int is_present_part = 2;
+		final int is_present_full = 1;
+		final int is_present_part = 2;
+		
 		int wage_per_hour = 20;
 		int working_hour = 0 ;
 		
-		double EmpCheck = Math.floor(Math.random() * 10) %3;
+		int EmpCheck = (int) (Math.random() * 10) %3;
 		
-		if (EmpCheck == is_present_full)
+		switch (EmpCheck)
 		{
+		case is_present_full:
 			System.out.println("Employee is present for full time");
 			working_hour = 8;
-		}
-		else if (EmpCheck == is_present_part)
-		{
+			break;
+			
+		case is_present_part:
 			System.out.println("Employee is present for part time");
 			working_hour = 4;
-		}
-		else
-		{
+			break;
+			
+		default:
 			System.out.println("Employee is absent");
 			working_hour = 0;
 		}
